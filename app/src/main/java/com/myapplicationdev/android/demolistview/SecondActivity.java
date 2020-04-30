5e0ef2f7b733ab14a2179b57cd4b94b60c588701
18044928 <18044928@myrp.edu.sg>
2020-04-30 10:46:36 +0800

    SecondActivity

--- app/src/main/java/com/myapplicationdev/android/demolistview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demolistview/SecondActivity.java
@@ -1,4 +1,21 @@
 package com.myapplicationdev.android.demolistview;
 
-public class SecondActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac);
+
+
+    }
 }
